775275d651bb631286f623186cd8308f6b12d218
k4abrego <188397826+k4abrego@users.noreply.github.com>
2026-05-21 11:50:16 -0600

    context free grammar

--- 7. Context-Free Grammars/grammar.clj
+++ 7. Context-Free Grammars/grammar.clj
@@ -1,11 +1,21 @@
+;----------------------------------------------------------
+; Problem Set #8: Context-Free Grammars
+; Date: May 29, 2026.
+; Authors:
+;          A01753979 Ana Karen Abrego Flores
+;          A01803514 Gabriel de Jesús Manzo Cuevas
+;----------------------------------------------------------
+
 (ns grammar
 (:require [clojure.test :refer [deftest is run-tests]])
 (:require [instaparse.core :refer [parser]])
 (:import (instaparse.gll Failure)))
 
+
 (defn fails? [r] (instance? Failure r))
 (defn succeeds? [r] (not (fails? r)))
 
+;Problem 1
 (def start-and-end (parser "
 
   S = '#'
@@ -19,6 +29,25 @@
 
 "))
 
+;Problem 2
+(def palindrome (parser "
+  P = epsilon
+  | '0'
+  | '1'
+  | '0' 'P' '0'
+  | '1' 'P' '1'
+ "))
+
+
+;Problem 3
+(def balanced-parentheses (parser "
+  P = epsilon
+  | '(' P ')' P
+"))
+
+
+;tests
+;1
 (deftest test-start-and-end
          (is (succeeds? (start-and-end "$")))
          (is (succeeds? (start-and-end "#")))
@@ -32,5 +61,39 @@
          (is (fails? (start-and-end "###$$#$#$$$#$$$####$")))
          (is (fails? (start-and-end "$#$#$#$$$$#$$$#$$$$#$$#")))
          (is (fails? (start-and-end "#######################$"))))
+
+;2
+(deftest test-palindrome
+         (is (succeeds? (palindrome "")))
+         (is (succeeds? (palindrome "0")))
+         (is (succeeds? (palindrome "1")))
+         (is (succeeds? (palindrome "11")))
+         (is (succeeds? (palindrome "00")))
+         (is (succeeds? (palindrome "010")))
+         (is (succeeds? (palindrome "1111111")))
+         (is (succeeds? (palindrome "000010000")))
+         (is (succeeds? (palindrome "01001110101110010")))
+         (is (fails? (palindrome "01")))
+         (is (fails? (palindrome "10")))
+         (is (fails? (palindrome "1010")))
+         (is (fails? (palindrome "10000000")))
+         (is (fails? (palindrome "00010001")))
+         (is (fails? (palindrome "1010011010")))
+         (is (fails? (palindrome "111111111111111111110"))))
+
+;3
+(deftest test-balanced-parentheses
+         (is (succeeds? (balanced-parentheses "")))
+         (is (succeeds? (balanced-parentheses "()")))
+         (is (succeeds? (balanced-parentheses "((((()))))")))
+         (is (succeeds? (balanced-parentheses "()()()()()")))
+         (is (succeeds? (balanced-parentheses
+                          "(()()())((())(()()()))(((())))")))
+         (is (fails? (balanced-parentheses "(")))
+         (is (fails? (balanced-parentheses ")")))
+         (is (fails? (balanced-parentheses "((((())))")))
+         (is (fails? (balanced-parentheses "))))((((")))
+         (is (fails? (balanced-parentheses
+                       "(()()())((())(()()()))((((())))"))))
 
 (run-tests)
