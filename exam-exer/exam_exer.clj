c45cd3b09654747ccbd472020c6a9b0750527260
k4abrego <188397826+k4abrego@users.noreply.github.com>
2026-06-11 20:06:36 -0600

    regex exercises

--- exam-exer/exam_exer.clj
+++ exam-exer/exam_exer.clj
@@ -69,3 +69,89 @@
 
 ;----------------------------------------------------------
 ;REGEX
+
+;Define un regex llamado ends-with-st que acepte strings sobre {s,t}
+;que terminan en st.
+
+(def ends-with-st #"[st]*st")
+
+(re-matches ends-with-st "sttstststst")
+
+
+
+;Define un regex llamado contains-at que acepte strings sobre {#, $, @}
+;que contienen al menos un @ .
+
+(def contains-at #"[#$@]*@[#$@]*")
+
+(re-matches contains-at "##$@")
+
+;Define un regex llamado exactly-three-digits que
+;acepte strings con exactamente tres dígitos.
+
+(def exactly-three-digits #"\d{3}")
+
+(re-matches exactly-three-digits "12")
+
+;Define un regex llamado one-or-more-digits que acepte uno o más dígitos.
+(def one-or-more-digits #"\d+")
+
+(re-matches one-or-more-digits "12")
+
+
+;Define un regex llamado optional-sign-integer que acepte un
+;entero con signo opcional + o -
+
+(def optional-sign-integer #"[+-]?\d+")
+
+(re-matches optional-sign-integer "+123")
+
+
+;Define un regex llamado word-then-digits que acepte una o más letras
+;minúsculas seguidas de uno o más dígitos.
+
+(def word-then-digits #"[a-z]+\d+")
+
+(re-matches word-then-digits "abc123")
+
+
+;Define un regex llamado tag-number que acepte un #, luego una o más
+;letras minúsculas, luego -, luego exactamente dos dígitos.
+
+(def tag-number #"[#][a-z]+[-]\d{2}")
+
+(re-matches tag-number "#x-99")
+
+
+
+;Define un regex llamado user-code que acepte: una o más letras
+; minúsculas luego @, luego exactamente 3 dígitos
+
+(def user-code #"[a-z]+[@]\d{3}")
+
+(re-matches user-code "ana@123")
+
+;Define un regex llamado money-code que acepte: un signo $ literal
+; luego una o más letras mayúsculas luego -, luego exactamente 2 dígitos
+
+(def money-code #"[$][A-Z]+-\d{2}")
+
+(re-matches money-code "$ABC-12")
+
+;Define un regex llamado paren-number que acepte: un paréntesis
+; izquierdo ( , luego exactamente 3 dígitos luego un paréntesis derecho )
+
+(def paren-number #"[\(]\d{3}[\)]")
+
+(re-matches paren-number "(123)")
+
+;Define un regex llamado scheme-radix que acepte enteros con prefijo Scheme:
+;#b = binario, solo 0 y 1
+;#o = octal, solo 0 a 7
+;#d = decimal, solo 0 a 9
+;#x = hexadecimal, 0 a 9, a-f, A-F
+
+(def scheme-radix #"#b[01]+|#o[0-7]+|#d[0-9]+|#x[0-9 a-f A-F]")
+
+(re-matches scheme-radixx "#b101")
+
